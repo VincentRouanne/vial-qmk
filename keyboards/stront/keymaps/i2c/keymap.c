e541325252a5dcb8d9f52bc92a985256972eb9d6
VincentRouanne <59823232+VincentRouanne@users.noreply.github.com>
2024-04-07 14:45:39 +0200

    Update keymap.c
    Add combos :
    - symbols
    - x/c/v/z/y
    - a and e accent
    Add tap dance x words caps

--- keyboards/stront/keymaps/i2c/keymap.c
+++ keyboards/stront/keymaps/i2c/keymap.c
@@ -17,6 +17,12 @@ enum layer_number {
     // clang-format on
 };
 
+enum combo_events {
+E_ACCENT,
+E_ACCENT2,
+A_ACCENT,
+};
+
 // Left-hand home row mods
 #define HOME_A LGUI_T(KC_A)
 #define HOME_O LALT_T(KC_O)
@@ -44,6 +50,62 @@ enum layer_number {
 #define LCTL_ESC LCTL_T(KC_ESC)
 #define LGUI_QUOT LGUI_T(KC_QUOT)
 
+// Define COMBOS!
+const uint16_t PROGMEM combo_lpar[] = {HOME_O, HOME_E, COMBO_END};
+const uint16_t PROGMEM combo_rpar[] = {HOME_E, HOME_U, COMBO_END};
+const uint16_t PROGMEM combo_lbra[] = {KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM combo_rbra[] = {KC_DOT, KC_P, COMBO_END}; 
+const uint16_t PROGMEM combo_lcbra[] = {KC_Q, KC_J, COMBO_END};
+const uint16_t PROGMEM combo_rcbra[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM combo_equal[] = {HOME_U, KC_I, COMBO_END};
+const uint16_t PROGMEM combo_semicol[] = {HOME_U, KC_K, COMBO_END};
+const uint16_t PROGMEM combo_und[] = {HOME_U, KC_P, COMBO_END};
+
+const uint16_t PROGMEM combo_0[] = {HOME_H, HOME_T, HOME_N, HOME_S, COMBO_END};
+const uint16_t PROGMEM combo_1[] = {KC_M, KC_W, COMBO_END};
+const uint16_t PROGMEM combo_2[] = {KC_W, KC_V, COMBO_END};
+const uint16_t PROGMEM combo_3[] = {KC_M, KC_W, KC_V, COMBO_END}; 
+const uint16_t PROGMEM combo_4[] = {HOME_H, HOME_T, COMBO_END};
+const uint16_t PROGMEM combo_5[] = {HOME_T, HOME_N, COMBO_END};
+const uint16_t PROGMEM combo_6[] = {HOME_H, HOME_T, HOME_N, COMBO_END};
+const uint16_t PROGMEM combo_7[] = {KC_G, KC_C, COMBO_END};
+const uint16_t PROGMEM combo_8[] = {KC_C, KC_R, COMBO_END};
+const uint16_t PROGMEM combo_9[] = {KC_G, KC_C, KC_R, COMBO_END};
+
+const uint16_t PROGMEM combo_accente[] = {HOME_E, KC_DOT, COMBO_END};
+const uint16_t PROGMEM combo_accente2[] = {HOME_E, KC_J, COMBO_END};
+const uint16_t PROGMEM combo_accenta[] = {HOME_A, KC_COLN, COMBO_END};
+
+combo_t key_combos[] = {
+    [E_ACCENT] = COMBO_ACTION(combo_accente), // put these first else they override the non-named combos
+    [E_ACCENT2] = COMBO_ACTION(combo_accente2),
+    [A_ACCENT] = COMBO_ACTION(combo_accenta),
+    COMBO(combo_lpar, KC_LPRN),
+    COMBO(combo_rpar, KC_RPRN),
+    COMBO(combo_lbra, KC_LBRC),
+    COMBO(combo_rbra, KC_RBRC),
+    COMBO(combo_lcbra, KC_LCBR),
+    COMBO(combo_rcbra, KC_RCBR),
+    COMBO(combo_equal, KC_EQL),
+    COMBO(combo_semicol, KC_SCLN),
+    COMBO(combo_und, KC_UNDS),
+    COMBO(combo_0, KC_0),
+    COMBO(combo_1, KC_1),
+    COMBO(combo_2, KC_2),
+    COMBO(combo_3, KC_3),
+    COMBO(combo_4, KC_4),
+    COMBO(combo_5, KC_5),
+    COMBO(combo_6, KC_6),
+    COMBO(combo_7, KC_7),
+    COMBO(combo_8, KC_8),
+    COMBO(combo_9, KC_9),
+};
+
+// doesn't work for now
+bool get_combo_must_tap(uint16_t index, combo_t *combo) {
+    return true;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* QWERTY
      *               .---------------------------.                    .---------------------------.
@@ -60,8 +122,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
     [_BASE] = LAYOUT(
                             KC_COMM, KC_DOT,  KC_P,    KC_Y,                KC_F,    KC_G,     KC_C,     KC_R,
-        KC_QUOT,   HOME_A,   HOME_O,  HOME_E,  HOME_U,  KC_I,               KC_D,    HOME_H,   HOME_T,   HOME_N,  HOME_S,  KC_L,
-        KC_COLN,  KC_COLN,  KC_Q,    KC_J,    KC_K,    KC_X,                KC_B,    KC_M,     KC_W,     KC_V,    KC_Z,    KC_Z,
+        KC_QUOT,  HOME_A,   HOME_O,  HOME_E,  HOME_U,  KC_I,                KC_D,    HOME_H,   HOME_T,   HOME_N,  HOME_S,  KC_L,
+        KC_COLN,  KC_COLN,  KC_Q,    KC_J,    KC_K,    TD(0),               KC_B,    KC_M,     KC_W,     KC_V,    KC_Z,    KC_Z,
                                      MOU_ESC, NAV_SPC, SYS_TAB,             SYM_ENT, NUM_BSPC, FUN_DEL
     ),
 
@@ -122,3 +184,75 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
     // clang-format on
 };
+
+// intercept mods on mod tap for copy/paste/etc on the nav layer
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LCTL_T(LCTL(KC_C)):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(LCTL(KC_C)); // Send copy on tap
+                return false;        // Return false to ignore further processing of key
+            }
+            break;
+
+        case LGUI_T(LCTL(KC_Y)):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(LCTL(KC_Y)); // Send redo on tap
+                return false;        // Return false to ignore further processing of key
+            }
+            break;
+
+        case LALT_T(LCTL(KC_Z)):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(LCTL(KC_Z)); // Send undo on tap
+                return false;        // Return false to ignore further processing of key
+            }
+            break;
+                
+        case LSFT_T(LCTL(KC_V)):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(LCTL(KC_V)); // Send paste on tap
+                return false;        // Return false to ignore further processing of key
+            }
+            break;
+    }
+    return true;
+}
+
+// CAPS_WORD on tap dance x
+void words_lock_k (tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        tap_code(KC_X);
+    } else if (state->count == 2) {
+        caps_word_on();
+    }
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+ [0] = ACTION_TAP_DANCE_FN (words_lock_k),
+};
+
+
+// Special Combos
+void process_combo_event(uint16_t combo_index, bool pressed) {
+  switch(combo_index) {
+    case E_ACCENT:
+      if (pressed) {
+        tap_code16(KC_QUOT);
+	tap_code16(KC_E);
+      }
+      break;
+    case E_ACCENT2:
+      if (pressed) {
+        tap_code16(KC_GRAVE);
+        tap_code16(KC_E);
+      }
+      break;
+    case A_ACCENT:
+      if (pressed) {
+        tap_code16(KC_GRAVE);
+        tap_code16(KC_A);
+      }
+      break;
+  }
+}
